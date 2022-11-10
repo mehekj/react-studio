e32a6b7fd0987be7b02a9bbccfd34646a140bc8d
mehekj <mehek.jethani@gmail.com>
2022-11-10 15:11:28 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,20 +10,33 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState([]);
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="browse-items">
+        <h1>My Bakery</h1>
+        <div className="bakery-grid">
+          {bakeryData.map((item, index) => (
+            <BakeryItem
+              item={item}
+              addToCart={(item) => setCart([...cart, item])}
+            />
+          ))}
+        </div>
+      </div>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
+      <div className="cart">
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cart
+          .filter((item, index) => cart.indexOf(item) === index)
+          .map((item) => (
+            <div>
+              {cart.filter((item2) => item === item2).length} {item.name}
+            </div>
+          ))}
+        <h3>Total</h3>$
+        {cart.reduce((sum, item) => (sum += item.price), 0).toFixed(2)}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,24 @@
-// TODO: create a component that displays a single bakery item
+import "../App.css";
+import { useState } from "react";
+
+export default function BakeryItem(props) {
+  return (
+    <div
+      className="BakeryItem"
+      onPointerDown={(e) => props.addToCart(props.item)}
+    >
+      <div className="item-img-wrapper">
+        <img src={props.item.image} alt=""></img>
+      </div>
+      <div className="item-text">
+        <p>
+          <b>{props.item.name}</b>
+        </p>
+        <p>{props.item.description}</p>
+        <p style={{ alignSelf: "flex-end", justifySelf: "flex-end" }}>
+          ${props.item.price}
+        </p>
+      </div>
+    </div>
+  );
+}
